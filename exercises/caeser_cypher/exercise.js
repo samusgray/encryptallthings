a13e17bbd010ef83e18bee11afbf522b4ad631af
Evan Moore <etmoore@gmail.com>
2016-03-24 15:58:29 -0600

    random arg for solution and submission

--- exercises/caeser_cypher/exercise.js
+++ exercises/caeser_cypher/exercise.js
@@ -3,6 +3,9 @@ var exercise      = require('workshopper-exercise')()
   , execute       = require('workshopper-exercise/execute')
   , comparestdout = require('workshopper-exercise/comparestdout')
 
+function getRandomIndex(arr) {
+  return Math.floor(Math.random() * arr.length)
+}
 
 // checks that the submission file actually exists
 exercise = filecheck(exercise)
@@ -12,6 +15,17 @@ exercise = execute(exercise)
 
 // compare stdout of solution and submission
 exercise = comparestdout(exercise)
+
+exercise.addSetup(function(mode, callback){
+  strings = ["It's the end of the world as we know it.",
+             "What's the matter with the clothes I'm wearing?",
+             "Never eat glue.",
+             "Dogs are the best."
+            ]
+  var arg = strings[getRandomIndex(strings)]
+  this.submissionArgs = this.solutionArgs = arg
+  process.nextTick(callback)
+})
 
 
 module.exports = exercise
